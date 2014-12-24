062d51e2929c93d10ff4326860afab155e59b386
MrAlun <850618956@qq.com>
2014-12-25 00:22:43 +0800

    要提交的变更： 	删除:         build/CMakeFiles/2.8.12.2/CMakeCCompiler.cmake 	删除:         build/CMakeFiles/2.8.12.2/CMakeCXXCompiler.cmake 	删除:         build/CMakeFiles/2.8.12.2/CMakeDetermineCompilerABI_C.bin 	删除:         build/CMakeFiles/2.8.12.2/CMakeDetermineCompilerABI_CXX.bin 	删除:         build/CMakeFiles/2.8.12.2/CMakeSystem.cmake 	删除:         build/CMakeFiles/2.8.12.2/CompilerIdC/CMakeCCompilerId.c 	删除:         build/CMakeFiles/2.8.12.2/CompilerIdC/a.out 	删除:         build/CMakeFiles/2.8.12.2/CompilerIdCXX/CMakeCXXCompilerId.cpp 	删除:         build/CMakeFiles/2.8.12.2/CompilerIdCXX/a.out 	删除:         build/CMakeFiles/CMakeDirectoryInformation.cmake 	删除:         build/CMakeFiles/cmake.check_cache 	删除:         build/CTestTestfile.cmake 	删除:         build/bin/CMakeFiles/CMakeDirectoryInformation.cmake 	删除:         build/bin/CMakeFiles/main.dir/DependInfo.cmake 	删除:         build/bin/CMakeFiles/main.dir/build.make 	删除:         build/bin/CMakeFiles/main.dir/cmake_clean.cmake 	删除:         build/bin/CMakeFiles/main.dir/depend.make 	删除:         build/bin/CMakeFiles/main.dir/flags.make 	删除:         build/bin/CMakeFiles/main.dir/link.txt 	删除:         build/bin/CMakeFiles/progress.marks 	删除:         build/bin/Makefile 	删除:         build/bin/cmake_install.cmake 	删除:         build/cmake_install.cmake 	删除:         build/lib/CMakeFiles/CMakeDirectoryInformation.cmake 	删除:         build/lib/CMakeFiles/hello.dir/DependInfo.cmake 	删除:         build/lib/CMakeFiles/hello.dir/build.make 	删除:         build/lib/CMakeFiles/hello.dir/cmake_clean.cmake 	删除:         build/lib/CMakeFiles/hello.dir/depend.make 	删除:         build/lib/CMakeFiles/hello.dir/flags.make 	删除:         build/lib/CMakeFiles/hello.dir/link.txt 	删除:         build/lib/CMakeFiles/hello.dir/progress.make 	删除:         build/lib/cmake_install.cmake

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/hgfs/share/server
-# Build directory: /mnt/hgfs/share/server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(mytest "/mnt/hgfs/share/server/build/bin/main")
-SUBDIRS(lib)
-SUBDIRS(bin)
