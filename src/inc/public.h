9f5f2eb4edde2cf8ee36fefd81d7a6c3aadf922f
MrAlun <850618956@qq.com>
2014-12-26 17:16:12 +0000

    Changes to be committed: 	modified:   src/inc/CMakeLists.txt 	modified:   src/inc/define/integer.h 	modified:   src/inc/public.h 	modified:   src/test/CMakeLists.txt

--- src/inc/public.h
+++ src/inc/public.h
@@ -1,6 +1,27 @@
 #ifndef _PUBLIC_H_
 #define _PUBLIC_H_
 
-#include <define/integer.h>
+#include "define/integer.h"
+
+// 最大值，最小值
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+// 禁止拷贝
+class NonCopyable  
+{  
+protected:  
+      NonCopyable() {}  
+      ~NonCopyable() {}  
+private:
+      NonCopyable( const NonCopyable& );  
+      const NonCopyable& operator=( const NonCopyable& );  
+};
+
+// 删除指针
+#define DELETE_VALUE(_ptr)	\
+	if((_ptr)) { delete (_ptr); (_ptr) = NULL; };
+#define DELETE_ARRAY(_ptr)	\
+	if((_ptr)) { delete[] (_ptr); (_ptr) = NULL; };
 
 #endif//_PUBLIC_H_
