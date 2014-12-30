8aa6de305420913a215ab1036bd3a0c0e09cb32d
MrAlun <850618956@qq.com>
2014-12-31 00:47:34 +0800

    要提交的变更： 	修改:         doc/config/test.xml 	修改:         src/test/server/server.cpp 	修改:         third/tinyxml/xmltest.cpp 	修改:         tools/xclean 	修改:         tools/xmake 	修改:         tools/xrun

--- src/test/server/server.cpp
+++ src/test/server/server.cpp
@@ -10,81 +10,62 @@
 
 #define SERVER_PORT 3000
 
+int32 port = 0;
+char ip[64] = "127.0.0.1";
+
 using namespace std;
+
+void Init()
+{
+	TiXmlDocument doc( "doc/config/test.xml" );
+	if ( !doc.LoadFile() )
+	{
+		cout << "xml load fail " << endl;
+		exit( -1 );
+	}
+
+	TiXmlElement* root = doc.RootElement();
+	TiXmlElement* first = root->FirstChildElement("server");
+	if ( first )
+	{
+		TiXmlElement* second = first->FirstChildElement("ip");
+		if ( second )
+			strcpy( ip, second->GetText() );
+
+		second = first->FirstChildElement("port");
+		if ( second )
+			port = atoi( second->GetText() );
+	}
+}
 
 int main( int argc, char* argv[] )
 {
-      TiXmlDocument doc( "doc/config/test.xml" );
-      if ( !doc.LoadFile() )
-      {
-            cout << "xml load fail " << endl;
-            exit( -1 );
-      }
-      //doc.Print( stdout );
-      //cout << doc;
-      {
-            //TiXmlPrinter printer;
-            //doc.Accept( &printer );
-            //fprintf( stdout, "%s", printer.CStr() );
-      }
+	Init();
       
-      {
-            TiXmlElement* RootElement = doc.RootElement();
-            cout << RootElement->Value() << endl;
-            TiXmlElement* FirstPerson = RootElement->FirstChildElement();
-            for ( ; NULL != FirstPerson ; FirstPerson = FirstPerson->NextSiblingElement() )
-            {
-                  cout << FirstPerson->Value() << endl;
-                  TiXmlAttribute* id = FirstPerson->FirstAttribute();
-                  for ( ; NULL != id ; id = id->Next() )
-                  {
-                        cout << id->Value() << endl;
-                        cout << id->Name() << endl;
-                  }
-            }
-            
-      }
-      
-      /*{
-            TiXmlNode* node = doc.FirstChild( "Root" );
-            assert( node );
-            TiXmlElement* todoElement = node->ToElement();
-            assert( todoElement  );
-		node = todoElement->FirstChildElement();
-		assert( node );
-		
-		int32 port = 0;
-		todoElement->Attribute( "port", &port );
-		const char* ip = todoElement->Attribute( "ip" );
-		printf( "%s:%d\n", ip, port );
-      }*/
-      
-      return 0;
-      
-      SOCKFD ssock = Socket( AF_INET, SOCK_STREAM, 0 );
-      if ( 0 > ssock )
+	SOCKFD ssock = Socket( AF_INET, SOCK_STREAM, 0 );
+	if ( 0 > ssock )
             exit( -1 );
             
-      struct sockaddr_in saddr= {0};
-      saddr.sin_family        = AF_INET;
-      saddr.sin_port          = htons( SERVER_PORT );
-      saddr.sin_addr.s_addr   = htonl( INADDR_ANY );
-      
-      int32 res = Bind( ssock, (Address*)&saddr, sizeof saddr );
-      if ( 0 > res )
-            exit( -1 );
+	struct sockaddr_in saddr= {0};
+	saddr.sin_family        = AF_INET;
+	saddr.sin_port          = htons( port );
+	saddr.sin_addr.s_addr   = htonl( INADDR_ANY );
+
+	int32 res = Bind( ssock, (Address*)&saddr, sizeof saddr );
+	if ( 0 > res )
+		exit( -1 );
             
-      res = Listen( ssock, 1024 );
-      if ( 0 > res )
-            exit( -1 );
+	res = Listen( ssock, 1024 );
+	if ( 0 > res )
+		exit( -1 );
             
-      for ( ; ; )
-      {
-            struct sockaddr_in caddr = {0};
+	for ( ; ; )
+	{
+		struct sockaddr_in caddr = {0};
 		socklen_t ca_len = sizeof(caddr);
 		SOCKFD csock = Accept( ssock, (Address*)&caddr, &ca_len );
 
-            pid_t childpid = Fork();
+		pid_t childpid = Fork();
 		if ( 0 == childpid ) 
 		{
 			close( ssock );
@@ -93,6 +74,6 @@ int main( int argc, char* argv[] )
 		}
 		close( csock );
 	}
-            
-      exit( 0 );
+	    
+	exit( 0 );
 }
