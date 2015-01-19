b9d94620e9433ac9fec78b794901a3a22444aabd
alun <850618956@qq.com>
2015-01-19 06:45:53 +0000

    Changes to be committed: 	modified:   src/inc/stream/stream.h

--- src/inc/stream/stream.h
+++ src/inc/stream/stream.h
@@ -14,21 +14,21 @@ public:
     Stream();
     ~Stream();
     
-    Self& operator<<( bool v );
-    Self& operator<<( int16 v );
-    Self& operator<<( uint16 v );
-    Self& operator<<( int32 v );
-    Self& operator<<( uint32 v );
-    Self& operator<<( int64 v );
-    Self& operator<<( uint64 v );
-    Self& operator<<( const void* v );
-    Self& operator<<( float v );
-    Self& operator<<( double v );
-    Self& operator<<( char v ) ;
-    Self& operator<<( const char* v );
-    Self& operator<<( const unsigned char* v );
-    Self& operator<<( const string& v );
-    Self& operator<<( const std::string& v );
+    Self& operator << ( bool v );
+    Self& operator << ( int16 v );
+    Self& operator << ( uint16 v );
+    Self& operator << ( int32 v );
+    Self& operator << ( uint32 v );
+    Self& operator << ( int64 v );
+    Self& operator << ( uint64 v );
+    Self& operator << ( const void* v );
+    Self& operator << ( float v );
+    Self& operator << ( double v );
+    Self& operator << ( int8 v ) ;
+    Self& operator << ( uint8 v ) ;
+    Self& operator << ( const int8* v );
+    Self& operator << ( const uint8* v );
+    Self& operator << ( const std::string& v );
     
 private:
     Buffer m_buffer;
