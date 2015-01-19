f6a9297e9b31fa88b2776f59123d842872ac3bf1
alun <850618956@qq.com>
2015-01-19 07:13:53 +0000

    Changes to be committed: 	modified:   src/inc/stream/stream.cpp

--- src/inc/stream/stream.cpp
+++ src/inc/stream/stream.cpp
@@ -7,3 +7,79 @@ Stream::Stream()
 Stream::~Stream()
 {
 }
+
+Self& Stream::operator << ( bool v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( int16 v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( uint16 v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( int32 v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( uint32 v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( int64 v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( uint64 v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( const void* v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( float v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( double v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( int8 v ) 
+{
+    return *this;
+}
+
+Self& Stream::operator << ( uint8 v ) 
+{
+    return *this;
+}
+
+Self& Stream::operator << ( const int8* v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( const uint8* v )
+{
+    return *this;
+}
+
+Self& Stream::operator << ( const std::string& v )
+{
+    return *this;
+}
+
