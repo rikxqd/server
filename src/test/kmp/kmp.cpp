c28462cdc9813444c10640b9ba2a7dd47366f64f
MrAlun <850618956@qq.com>
2015-01-21 21:14:29 +0800

    要提交的变更： 	修改:         src/test/kmp/kmp.cpp

--- src/test/kmp/kmp.cpp
+++ src/test/kmp/kmp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 #include "str/str_func.h"
 
@@ -8,5 +9,9 @@ int main( int argc, char** argv )
     const char* a = "abcdddwafswfwalomcp";
     const char* b = "wafswf";
     std::cout << StrFunc::KmpSearch( a, b ) << std::endl;
+
+    char c[64] = {0};
+    strncpy( c, a + StrFunc::KmpSearch( a, b ), strlen( b ) );
+    std::cout << c << std::endl;
    return 0; 
 }
